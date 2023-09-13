1b914f201241b39b5bea69f7679ec50973e40346
Ryan <ryancpereira@protonmail.com>
2023-09-12 21:49:46 -0300

    Adding some binary operators

--- test.exs
+++ test.exs
@@ -1,10 +1,14 @@
-double_number = &(&1 * 2)
-4 |> double_number.() |> IO.puts()
+# double_number = &(&1 * 2)
+# 4 |> double_number.() |> IO.puts()
 
-defmodule Ryan do
-  def zin do
-    "Hell Yeah!"
+defmodule BinaryOperators do
+  def add lhs, rhs do
+    lhs + rhs
+  end
+
+  def sub lhs, rhs do
+    lhs - rhs
   end
 end
 
-Ryan.zin() |> IO.puts()
+BinaryOperators.add(4,6) |> BinaryOperators.sub(2) |> IO.puts()
